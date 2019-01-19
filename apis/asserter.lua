3eef50ad38f67117de784e71aad252b6bca46a77
Yimo Awanardo <awanardo@protonmail.ch>
2019-01-19 17:54:23 +0100

    Getting the structure together

---
+++ apis/asserter.lua
@@ -0,0 +1,15 @@
+local asserter = {}
+
+function assert_form(val, exp)
+  local result = true;
+  if type(val) == type(exp) then
+    if type(val) == "table" then
+
+    else
+
+    end
+  end
+  return result;
+end
+
+return asserter;
---
+++ apis/lines.lua
@@ -0,0 +1,12 @@
+local lines = {}
+lines.line = {
+}
+
+function lines:new_line(...)
+  local m = {...}
+  if type(m[1])=="table" and type(m[2])=="table" and type(m[1][1])=="number" then
+
+end
+function lines.get_intersection()
+
+return lines;
